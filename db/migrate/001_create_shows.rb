db72e9030c3a57cfb8551882bab7f75a0099b027
Candice Peters <candicelizabethpeters@gmail.com>
2019-11-18 06:15:56 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -2,7 +2,7 @@ class CreateShows < ActiveRecord::Migration[5.2]
   def change
     create_table :shows do |t|
       t.string :name
-      t.integer :network
+      t.string :network
       t.string :day
       t.integer :rating
   end
